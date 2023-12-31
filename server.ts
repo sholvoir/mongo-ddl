1183eece74a08301e5d015e97e3c4417a4a97839
sholvoir <sovar.he@gmail.com>
2023-12-30 21:11:33 -0500

    ext

--- server.ts
+++ server.ts
@@ -18,15 +18,22 @@ interface IndexInfo {
 
 export async function handler(req: Request) {
     try {
+        if (req.method != 'POST') return new Response('Only accept POST method', { status: Status.BadRequest });
         const token = req.headers.get('Authorization')?.match(/Bearer (.*)/)?.at(1);
         if (!token || !await jwt.verifyToken(token)) return new Response(undefined, { status: Status.Forbidden });
-        const body = await req.json() as IndexInfo;
         const client = new MongoClient();
         await client.connect(uri);
-        const collection = client.database(body.database).collection(body.collection);
-        await collection.createIndexes({ indexes: body.indexes });
-        client.close();
-        return new Response(undefined, { status: Status.OK });
+        switch (new URL(req.url).pathname) {
+            case "/create-index": {
+                const body = await req.json() as IndexInfo;
+                const collection = client.database(body.database).collection(body.collection);
+                await collection.createIndexes({ indexes: body.indexes });
+                client.close();
+                return new Response(undefined, { status: Status.OK });
+            }
+            default: return new Response(undefined, { status: Status.BadRequest });
+        }
+        
     } catch (e) {
         console.log(e);
         return new Response(JSON.stringify(e), { status: Status.BadRequest });
